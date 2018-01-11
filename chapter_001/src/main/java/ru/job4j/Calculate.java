1438d490374acca3a7d1d6be799cad9fe59edb60
zkhwtf <zkhwtf@mm.st>
2018-01-11 20:46:27 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,16 @@
 package ru.job4j;
 
+/**
+ * Клас, вывод строки в консоль.
+ * @author Pavel Zakhvatov (zkhwtf@mm.st)
+ * @since 11.01.18
+ * @version 1
+ */
 public class Calculate {
+  /**
+   * Метод, вывод строки в консоль.
+   * @param args - args, имя массива  String.
+   */
   public static void main(String[] args) {
     System.out.println("Hello, World!!");
   }
